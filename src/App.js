9cf51e908cb212369fd25793a230e99752f7a49a
payal <payal08102002@gmail.com>
2024-07-24 00:07:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { AnimeList } from "./AnimeList";
+import { AnimeInfo } from "./AnimeInfo";
+import { AddToList } from "./AddToList";
+import {RemoveFromList} from "./RemoveFromList";
+
+function App() {
+
+  const [search, setSearch] = useState('Naruto')
+  const [animeData, setAnimeData] = useState();
+  const [animeInfo, setAnimeInfo] = useState()
+  const[myAnimeList,setMyAnimeList]=useState([])
+
+  const addTo=(anime)=>{
+    const index=myAnimeList.findIndex((myanime)=>{
+        return myanime.mal_id === anime.mal_id
+    })
+    if(index < 0){
+      const newArray=[...myAnimeList,anime]
+      setMyAnimeList(newArray);
+    }
+    
+  }
+  const removeFrom=(anime)=>{
+    const newArray=myAnimeList.filter((myanime)=>{
+      return myanime.mal_id !== anime.mal_id
+    })
+    setMyAnimeList(newArray)
+  }
+  const getData = async () => {
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=25`)
+    const resData = await res.json();
+    setAnimeData(resData.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [search])
+
+  return (
+    <>
+      <div className='header'>
+        <h1 className='logo'>MyAnime </h1>
+        <div className='search-box'>
+          <input type='search' placeholder='Search Your Anime' 
+          onChange={(e) => setSearch(e.target.value)}></input>
+        </div>
+      </div>
+      <div className="container">
+        <div className="animeInfo">
+        {animeInfo && <AnimeInfo animeInfo={animeInfo} />}
+        </div>
+        <div className="anime-row">
+          <h2 className='text-heading'>Anime</h2>
+          <div className="row">
+            <AnimeList 
+            animelist={animeData}
+            setAnimeInfo={setAnimeInfo}
+            animeComponent={AddToList}
+            HandleList={(anime)=>addTo(anime)}
+            />
+
+          </div>
+          <h2 className='text-heading'>My List</h2>
+          <div className="row">
+            <AnimeList 
+            animelist={myAnimeList}
+            setAnimeInfo={setAnimeInfo}
+            animeComponent={RemoveFromList}
+            HandleList={(anime)=>removeFrom(anime)}
+            />
+
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
